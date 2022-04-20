097f3d2c138655702fa9cc86b25411147353a527
GabrielRezes <gabriel.rezes@gmail.com>
2022-04-20 11:16:39 -0300

    adding theme provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
 import { ThemeProvider } from 'styled-components';
 import styled from 'styled-components';
 import AppRoutes from './routes/AppRoutes';
+
+import Header from './components/Header';
 import Navbar from './components/Navbar';
 
 const theme = {
   colors: {
-    base: '#ECECF1', 
+    base: '#f2f2f5', 
     pink:'#EE5E69',
     pinkGradient: 'linear-gradient(90deg, rgba(238,166,171,1) 0%, rgba(238,94,105,1) 100%)',
     gray: '#B9BBC3'
   },
+
+  size: {
+    sm: '330px',
+    md: '700px',
+    lg: '999px',
+    xl: '1700px'
+  }
 };
 
 const Container = styled.div`
   display: grid;
-  grid-template-rows: 2fr;
+  grid-template-rows: 80px 1fr 60px;
+  overflow-y: auto;
   height: 100vh;
-  padding: 5px;
+  margin: 0 auto;
+  padding: 10px;
+  max-width: 1300px;
+  border: 3px solid #000;
   background: ${theme.colors.base};
 `
 
@@ -24,6 +37,7 @@ export default function App() {
   return (
   <ThemeProvider theme={theme}>
     <Container>
+      <Header/>
       <AppRoutes/>
       <Navbar/>
     </Container>
