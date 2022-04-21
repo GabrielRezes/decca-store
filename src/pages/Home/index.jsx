4304698c35a91782627256c6d94571ff954edeb6
GabrielRezes <gabriel.rezes@gmail.com>
2022-04-20 23:24:32 -0300

    remove console log

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -10,72 +10,73 @@ import { BiSearchAlt } from 'react-icons/bi';
 
 const ContainerHome = styled.main`
   display: grid;
+  place-items: center;
   grid-template-rows: 40px 1fr;  
-  padding: 10px 0;
+  padding: 10px 10px;
+  /* background: red; */
 `;
 
 const ContainerSearch = styled.div`
-display: flex;
-align-items: center;
-padding: 5px;
-background: #fff;
-border-radius: 30px;
-box-shadow: 0px 2px 20px 10px rgba(230, 230, 230, 0.9);
-width: 100%;
-gap: 10px;
-font-size: 30px;
+  display: flex;
+  align-items: center;
+  padding: 5px;
+  background: #fff;
+  border-radius: 30px;
+  box-shadow: 0px 2px 20px rgba(230, 230, 230, 0.7);
+  width: 100%;
+  max-width: 330px;
+  gap: 10px;
+  font-size: 30px;
 
-svg {
-  color: ${props => props.theme.colors.pink} 
-}
+  svg {
+    color: ${props => props.theme.colors.pink} 
+  }
 
-select {
-  border: none;
-  background: inherit;
-  flex-grow: 1
-}
-`
+  select {
+    border: none;
+    background: inherit;
+    flex-grow: 1
+  }
+`;
 
 const category =['Pulseiras', 'Anéis', 'Colares', 'Brincos Pequenos', 'Brincos Grandes', 'Tornozeleiras' ]
 
 export default function Home () {
 
-  let arr = [
-    {img: a},
-    {img: a},
-    {img: a},
-    {img: b},
-    {img: b},
-    {img: b},
-    {img: c},
-    {img: c},
-    {img: c},
-    {img: d},
-    {img: d},
-    {img: d},
-    {img: e},
-    {img: e},
-    {img: e},
-
-  ]
+let arr = [
+  {img: a},
+  {img: a},
+  {img: a},
+  {img: b},
+  {img: b},
+  {img: b},
+  {img: c},
+  {img: c},
+  {img: c},
+  {img: d},
+  {img: d},
+  {img: d},
+  {img: e},
+  {img: e},
+  {img: e},
+];
 
   return (
     <ContainerHome>
 
-    <ContainerSearch>
-      <BiSearchAlt/> 
-        <select>
-          <option value="" disabled selected>Escolha a categoria</option>
-          {category.map(i => <option>{i}</option>)}
-        </select>
-    </ContainerSearch>
+      <ContainerSearch>
+        <BiSearchAlt/> 
+          <select>
+            <option value="" disabled selected>Escolha a categoria</option>
+            {category.map(i => <option>{i}</option>)}
+          </select>
+      </ContainerSearch>
 
-      {arr.map(i => {
-        return (
-          <Card image={i.img}/>
-        )
-      })}
-
+        {arr.map(i => {
+          return (
+            <Card image={i.img}/>
+          )
+        })}
 
     </ContainerHome>
   );
