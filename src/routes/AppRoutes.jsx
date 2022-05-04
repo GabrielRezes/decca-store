7960f9652ebe9946c295bcd2839518e0ac367642
GabrielRezes <gabriel.rezes@gmail.com>
2022-05-03 23:17:47 -0300

    adding new route

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from '../pages/Home';
 import Login from '../pages/Login';
+import AdmProducts from '../pages/AdmProducts'
 
 export default function AppRoutes () {
   return (
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/login' element={<Login/>}/>
+        <Route path='/admProducts' element={<AdmProducts/>}/>
       </Routes>
   );
 };
